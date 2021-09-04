b1f78d9d61bf979ee32a379f052f484b0897f1e3
jjcourtney <jcourtneywork@gmail.com>
2021-09-04 21:58:34 +0100

    bug fixed issue in keys

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -13,7 +13,7 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    category_id: {
+    product_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'product',
@@ -21,7 +21,7 @@ ProductTag.init(
         unique: false
       }
     },
-    category_id: {
+    tag_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'tag',
